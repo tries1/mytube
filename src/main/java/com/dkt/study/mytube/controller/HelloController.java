0eddbd36f923cd1d22985f2bd363f27db849d5c9
glenn <glenn.jo@kakaocorp.com>
2018-04-10 11:38:49 +0900

    add logback

--- src/main/java/com/dkt/study/mytube/controller/HelloController.java
+++ src/main/java/com/dkt/study/mytube/controller/HelloController.java
@@ -3,14 +3,19 @@ package com.dkt.study.mytube.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     //@ResponseBody
     @GetMapping("hello")
     public Mono<String> hello(){
+
+        log.debug("test : {}", 123);
+
         return Mono.just("hello1111");
     }
 }
